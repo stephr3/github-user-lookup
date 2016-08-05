9a2c7113faa2649619749a7d2f751af94d18f50d
Stephanie Gurung <stephr3@gmail.com>
2016-08-05 08:49:28 -0700

    connect with github API and display user repos in console

--- js/user.js
+++ js/user.js
@@ -1,5 +1,16 @@
+var apiKey = require('./../.env').apiKey;
+
 function User(username) {
   this.username = username;
 }
+
+User.prototype.getRepos = function(){
+  var self = this;
+  $.get('https://api.github.com/users/' + self.username + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
 
 exports.userModule = User;
--- js/usersearch-interface.js
+++ js/usersearch-interface.js
@@ -1,5 +1,11 @@
 var User = require('./../js/user.js').userModule;
 
 $(document).ready(function() {
-
+  $("#username-input").click(function(event){
+    event.preventDefault();
+    username = $("#username").val();
+    newUserSearch = new User(username);
+    console.log(newUserSearch);
+    newUserSearch.getRepos();
+  });
 });
