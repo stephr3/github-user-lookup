223178a0831d74380439ed6de23be9160098ebe3
Stephanie Gurung <stephr3@gmail.com>
2016-08-05 09:44:29 -0700

    initialize sass styling, add error message response, and hide old search results when a new search is submitted

--- js/usersearch-interface.js
+++ js/usersearch-interface.js
@@ -3,7 +3,10 @@ var User = require('./../js/user.js').userModule;
 $(document).ready(function() {
   $("#username-input").click(function(event){
     event.preventDefault();
+    $("#repo-list").empty();
+    $("#error").hide();
     username = $("#username").val();
+    $("#username").val("");
     $("#username-result").text(username);
     newUserSearch = new User(username);
     newUserSearch.getRepos();
